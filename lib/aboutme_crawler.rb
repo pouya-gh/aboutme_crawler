e8a83484b0628cd7df2038cc7e5a793fae65a683
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-10-01 21:12:47 +0330

    require needed libraries

--- lib/aboutme_crawler.rb
+++ lib/aboutme_crawler.rb
@@ -1,4 +1,9 @@
 require "aboutme_crawler/version"
+require 'watir-webdriver'
+require 'watir-scroll'
+require 'nokogiri'
+require 'json'
+require 'set'
 
 module AboutmeCrawler
   # Your code goes here...
