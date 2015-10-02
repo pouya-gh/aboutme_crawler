3e457e149d7386007afb78addcdc48eaaddedcd6
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-10-02 10:11:38 +0330

    make gem skeleton

--- lib/aboutme_crawler.rb
+++ lib/aboutme_crawler.rb
@@ -7,5 +7,35 @@ require 'json'
 require 'set'
 
 module AboutmeCrawler
-  # Your code goes here...
-end
+  class Crawler
+    def initialize
+
+    end
+
+    def initialize(settings_hash)
+
+    end
+
+    def search_for(q)
+
+    end
+
+    def crawl_profiles(list_of_links)
+
+    end
+
+    private
+
+    def extract_profile_links(html_doc, profile_links_set, number_of_results, max_results)
+
+    end
+
+    def extract_contacts(html_doc)
+
+    end
+
+    def read_settings_from_file
+
+    end
+  end # end of Crawler
+end # end of AboutmeCrawler
