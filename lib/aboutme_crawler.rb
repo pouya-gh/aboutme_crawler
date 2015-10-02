f425671b4f191c398dd9ca3c04a9681d1f7e3ba6
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-10-02 12:48:59 +0330

    implement sign_in and search_for

--- lib/aboutme_crawler.rb
+++ lib/aboutme_crawler.rb
@@ -30,11 +30,47 @@ module AboutmeCrawler
     end
 
     def sign_in
-
+      # @browser.goto "about.me"
+      # sleep(@step_delay)
+      @browser.goto "about.me/login"
+      sleep(@step_delay)
+      @browser.text_field(id: 'login').set(@username)
+      @browser.text_field(id: 'password').set(@password)
+      @browser.checkbox(id: 'remember').clear
+      @browser.button(class: 'button-submit').click
+      sleep(@step_delay)
     end
 
     def search_for
+      output_file = File.open("./output/profiles.json", 'w')
+      links_set = Set.new
+      number_of_results = 0
 
+      @browser.goto "about.me/search"
+      sleep(@step_delay)
+      @browser.text_field(id: 'terms').set(q)
+      sleep(@step_delay)
+      @browser.button(class: 'submitbutton').click
+      sleep(@step_delay)
+
+      if @browser.p(class: 'search-number').present?
+        number_of_results = @browser.p(class: 'search-number').text.gsub(/[^\d]/i, '').to_i
+      end
+      if number_of_results == 0
+        puts "Nothing was found!"
+        return
+      end
+
+      while links_set.size < @max_results && links_set.size < number_of_results
+        @browser.scroll.to :bottom
+        sleep(@step_delay)
+
+        html_doc = "<html><head></head><body>" + browser.div(class: 'search-results-container').when_present(100).html + "</body></html>"
+        extract_profile_links(html_doc, links_set, number_of_results)
+      end
+
+      output_file.puts links_set.to_a.to_json
+      output_file.close
     end
 
     def crawl_profiles
