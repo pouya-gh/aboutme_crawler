f00946ae0ad4a442bde1842354e2fdfa1199cbc1
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-10-02 12:23:35 +0330

    implement initializers

--- lib/aboutme_crawler.rb
+++ lib/aboutme_crawler.rb
@@ -9,11 +9,24 @@ require 'set'
 module AboutmeCrawler
   class Crawler
     def initialize
-
+      initialize(read_settings_from_file)
     end
 
     def initialize(settings_hash)
+      client = Selenium::WebDriver::Remote::Http::Default.new
+      profile = Selenium::WebDriver::Firefox::Profile.new
+      proxy = {}
+      proxy[:http] = settings_hash[:http_proxy] if settings_hash[:http_proxy] != ''
+      proxy[:ssl] = settings_hash[:ssl_proxy] if settings_hash[:ssl_proxy] != ''
+      profile.proxy = Selenium::WebDriver::Proxy.new proxy
+      client.timeout = settings_hash[:timeout].to_i > 0 ? settings_hash[:timeout].to_i : DEFAULT_TIMEOUT
 
+      @username = settings_hash[:username]
+      @password = settings_hash[:password]
+      @step_delay = settings_hash[:step_delay].to_i > 0 ? settings_hash[:step_delay].to_i : DEFAULT_STEP_DELAY
+      @search_query = settings_hash[:search_query] != '' ? settings_hash[:search_query] : DEFAULT_SEARCH_QUERY
+      @max_results = settings_hash[:max_results].to_i > 0 ? settings_hash[:max_results].to_i : DEFAULT_MAX_RESULTS
+      @browser = Watir::Browser.new :firefox, profile: profile, http_client: client
     end
 
     def search_for(q)
